b7ab315ee61967e5ce303d414f40e7190882c072
Dave Thompson <>
2025-05-22 06:07:20 +0100

    Sentence Case customisation

--- aW7eb/keymap.c
+++ aW7eb/keymap.c
@@ -173,6 +173,45 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+char sentence_case_press_user(uint16_t keycode,
+                              keyrecord_t* record,
+                              uint8_t mods) {
+  if ((mods & ~(MOD_MASK_SHIFT | MOD_BIT(KC_RALT))) == 0) {
+    const bool shifted = mods & MOD_MASK_SHIFT;
+    switch (keycode) {
+      case KC_A ... KC_Z:
+        return 'a';  // Letter key.
+
+      case KC_DOT:  // . is punctuation, Shift . is a symbol (>)
+        return !shifted ? '.' : '#';
+      case KC_1:
+      case KC_SLSH:
+        return shifted ? '.' : '#';
+      case KC_EXLM:
+      case KC_QUES:
+        return '.';
+      case KC_2 ... KC_0:  // 2 3 4 5 6 7 8 9 0
+      case KC_AT ... KC_RPRN:  // @ # $ % ^ & * ( )
+      case KC_MINS ... KC_SCLN:  // - = [ ] backslash ;
+      case KC_UNDS ... KC_COLN:  // _ + { } | :
+      case KC_GRV:
+      case KC_COMM:
+        return '#';  // Symbol key.
+
+      case KC_SPC:
+      case KC_ENTER: // DT Cusomisation - Treat enter as a space, thus capitalising the start of paragraphs.
+        return ' ';  // Space key.
+
+      case KC_QUOT:
+        return '\'';  // Quote key.
+    }
+  }
+
+  // Otherwise clear Sentence Case to initial state.
+  sentence_case_clear();
+  return '\0';
+}
+
 uint8_t layer_state_set_user(uint8_t state) {
     uint8_t layer = biton(state);
   ergodox_board_led_off();
