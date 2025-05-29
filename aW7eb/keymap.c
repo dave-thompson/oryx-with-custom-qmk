d492835eef2c6c023a02749e8689786f86e8e2dc
Dave Thompson <>
2025-05-29 06:25:22 +0100

    Debug

--- aW7eb/keymap.c
+++ aW7eb/keymap.c
@@ -130,7 +130,7 @@ void set_layer_color(int layer) {
 }
 
 bool rgb_matrix_indicators_user(void) {
-  return false;
+  return true;
   // if (keyboard_config.disable_layer_led) { return false; }
   // switch (biton32(layer_state)) {
   //   case 0:
