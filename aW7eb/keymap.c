4255c6fa39bc7a15155e9f7d064507c6238f9490
Dave Thompson <>
2025-05-29 11:51:41 +0100

    Debug

--- aW7eb/keymap.c
+++ aW7eb/keymap.c
@@ -201,8 +201,6 @@ char sentence_case_press_user(uint16_t keycode,
                               keyrecord_t* record,
                               uint8_t mods) {  
   uprintf("SC key: %u\n", keycode); // TODO: Remove
-  uprintf("KC_EXLM is: %u\n", KC_EXLM); // TODO: Remove
-  uprintf("KC_QUES is: %u\n", KC_QUES); // TODO: Remove
 
   if ((mods & ~(MOD_MASK_SHIFT | MOD_BIT(KC_RALT))) == 0) {
     const bool shifted = mods & MOD_MASK_SHIFT;
@@ -213,17 +211,13 @@ char sentence_case_press_user(uint16_t keycode,
         return 'a';  // Letter key.
 
       case KC_DOT:  // . is punctuation, Shift . is a symbol (>)
-        uprintf("SC: DOT, S/U"); // TODO: Remove
-        return !shifted ? '.' : '#';
+        return '.'; // DT Cutomisation - Shift-. is  ! rather than >
       
-      case KC_1:
-      case KC_SLSH:
-        uprintf("SC: 1 or /, S/U"); // TODO: Remove
-        return shifted ? '.' : '#';
+      case KC_COMM:
+        return shifted ? '.' : '#'; // DT Cutomisation - Shift-, is  ? rather than <
       
       case KC_EXLM:
       case KC_QUES:
-        uprintf("SC: !/?"); // TODO: Remove
         return '.';
       
       case KC_2 ... KC_0:  // 2 3 4 5 6 7 8 9 0
@@ -231,7 +225,6 @@ char sentence_case_press_user(uint16_t keycode,
       case KC_MINS ... KC_SCLN:  // - = [ ] backslash ;
       case KC_UNDS ... KC_COLN:  // _ + { } | :
       case KC_GRV:
-      case KC_COMM:
         return '#';  // Symbol key.
       
       case KC_SPC:
