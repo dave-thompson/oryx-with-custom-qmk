f6225e8703a1ea5bce20f4c7f9777577d29b9965
Dave Thompson <>
2025-05-29 09:45:47 +0100

    Debug

--- aW7eb/keymap.c
+++ aW7eb/keymap.c
@@ -3,6 +3,7 @@
 
 #include "features/sentence_case.h"
 #include "features/custom_shift_keys.h"
+#include "print.h" // TODO: Uncomment
 //#include "features/select_word.h" // TODO: Uncomment
 
 const custom_shift_key_t custom_shift_keys[] = {
@@ -199,6 +200,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 char sentence_case_press_user(uint16_t keycode,
                               keyrecord_t* record,
                               uint8_t mods) {
+  uprintf("kc: %s\n", get_keycode_string(keycode)); // TODO: Remove
   if ((mods & ~(MOD_MASK_SHIFT | MOD_BIT(KC_RALT))) == 0) {
     const bool shifted = mods & MOD_MASK_SHIFT;
     switch (keycode) {
